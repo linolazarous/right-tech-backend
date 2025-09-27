825a36bcdf7de9181f62bfa71c6aadb7442545bf
linolazarous <linolaku560@gmail.com>
2025-09-27 13:02:22 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
-import App from './App';
+import App from './App.js';
 import initSentry from './utils/sentry'; 
 import './i18n/config';
 import './assets/styles/global.css';
@@ -45,3 +45,4 @@ root.render(
     </HelmetProvider>
   </React.StrictMode>
 );
+
