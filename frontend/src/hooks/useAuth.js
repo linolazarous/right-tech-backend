eec4e517b9610fb2b925c0d6c782f427e7b2c073
linolazarous <linolaku560@gmail.com>
2025-09-27 13:26:43 +0200

    Update useAuth.js

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext, createContext } from 'react';
-import api from '../services/api';
+import api from '../services/api.js';
 
 // Create Auth Context
 export const AuthContext = createContext();
