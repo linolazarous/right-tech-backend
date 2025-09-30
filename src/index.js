eff168ffbdb77ef740b66fec6db26771c32af7fa
linolazarous <linolaku560@gmail.com>
2025-09-30 23:42:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import App from './App';
-import './i18n/config'; // Import i18n configuration
-import './assets/styles/global.css';
-
-
-// IMPORTANT: Replace this with your actual reCAPTCHA v3 Site Key
-const RECAPTCHA_SITE_KEY = process.env.REACT_APP_RECAPTCHA_SITE_KEY || 'YOUR_RECAPTCHA_V3_SITE_KEY';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <GoogleReCaptchaProvider 
-          reCaptchaKey={RECAPTCHA_SITE_KEY}
-          scriptProps={{
-            async: false,
-            defer: false,
-            appendTo: 'head',
-            nonce: undefined,
-          }}
-        >
-          <App />
-        </GoogleReCaptchaProvider>
-      </BrowserRouter>
-    </HelmetProvider>
+    <App />
   </React.StrictMode>
 );
-
-
-
-
-
-
