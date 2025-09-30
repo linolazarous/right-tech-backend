ceecbbb4823a0b82822e1fd5f63604832e75be2f
linolazarous <linolaku560@gmail.com>
2025-09-30 23:57:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SimpleErrorBoundary from './components/SimpleErrorBoundary';
 import HomePage from './pages/HomePage';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <SimpleErrorBoundary>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </SimpleErrorBoundary>
   );
 }
 
