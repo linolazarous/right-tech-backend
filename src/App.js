1fca07415a9082e337667f1ceeab7fbe8b7e0b84
linolazarous <linolaku560@gmail.com>
2025-09-30 23:41:18 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import CoursePage from './pages/CoursePage';
-import ProfilePage from './pages/ProfilePage';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/courses" element={<CoursePage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          {/* Add other routes as needed */}
-        </Routes>
-      </main>
-      <Footer />
-      <Toaster position="top-center" reverseOrder={false} />
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Right Tech Centre</h1>
+      <p>If you can see this, React is working!</p>
+      <button onClick={() => alert('It works!')}>Test Button</button>
     </div>
   );
 }
